5e699c96c05e463aebdba8909baa14594a72c559
kkithome <117781574+kkithome@users.noreply.github.com>
2025-03-06 23:20:21 -0500

    added a menubar still needs functionality, storyboards, and personas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMConetenLoaded", function () {
     const questions = document.querySelectorAll(".question");
 
@@ -16,3 +17,29 @@ document.addEventListener("DOMConetenLoaded", function () {
         });
     });
 });
+
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() { 
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+        
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(
+    function(menuItem) {
+        menuItem.addEventListener("click", toggleMenu);
+    })
