23bf51c71b151991d0fb11bd6c30152cd32c8d5d
kkithome <117781574+kkithome@users.noreply.github.com>
2025-03-09 09:26:15 -0400

    menu has partial functionality, q&A still not working

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const questions = document.querySelectorAll(".question");
 
-document.addEventListener("DOMConetenLoaded", function () {
-    const questions = document.querySelectorAll(".question");
+  questions.forEach((question) => {
+    question.addEventListener("click", function () {
+        const questionContainer = this.closest('.question-container');
+        const answerContainer = questionContainer.querySelector('answer-container');
 
-    questions.forEach((questions, index) => {
-        questions.addEventListener("click", function () {
-            const answer = this.nextElementSibling;
-            answer.classList.toggle("visible");
+
+        if (answerContainer && answerContainer.classList.contains("answer-container")) {
+            answerContainer.classList.toggle("visible");
 
             const arrow = this.querySelector(".arrow");
-
-            if (answer.classList.contain("visible")) {
-                arrow.textContent = " 🔼 "; 
+            
+            if (answerContainer.classList.contains("visible")) {
+                arrow.textContent = " 🔼 ";
             } else {
                 arrow.textContent = " 🔽 ";
             }
-        });
+        }
     });
 });
-
+});
 
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
 const menuIcon = document.querySelector(".menuIcon");
+const closeIcon = document.querySelector(".closeIcon");
 
-function toggleMenu() { 
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-        
-    } else {
-        menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener("click", function() {
+    toggleMenu();
+});
 
-menuItems.forEach(
-    function(menuItem) {
-        menuItem.addEventListener("click", toggleMenu);
-    })
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener("click", toggleMenu);
+});
